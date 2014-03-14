0f9495305ebec50d40d6adae7a7c50e1b5d60815
Chris Thorn <chris@koding.com>
2014-03-13 19:55:49 -0700

    special handling for bluebird removed
    a few days ago we needed this because of a bug in bluebird which
    has since been fixed.  This commit should also fix an issue with
    the npm package.

--- bluebird.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd node_modules
-git clone git@github.com:petkaantonov/bluebird.git
-cd bluebird
-npm install
-node_modules/.bin/grunt build --features="core filter map reduce nodeify timers"
