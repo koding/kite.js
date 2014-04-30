167bd65d208ed8c9c53749fbdb80951f98955511
humanchimp <morphcham@gmail.com>
2014-04-30 15:21:41 -0700

    examples: add verbose mode to kontrol example

--- examples/kontrol/kontrol.js
+++ examples/kontrol/kontrol.js
@@ -1,35 +1,34 @@
+var argv = require('minimist')(process.argv);
 var fs = require('fs');
 var joinPath = require('path').join;
 
-var log = console.log.bind(console)
-  , warn = console.warn.bind(console)
-;
+var log = console.log.bind(console);
+var warn = console.warn.bind(console);
 
 var logLevels = require('../../logging').logLevels;
 
 var KiteServer = require('../../server');
 
-var fetchRegistrationIp = require('../fetch-registration-ip.js')
-
 var math = new KiteServer({
-  name: 'math',
-  username: 'koding',
-  environment: 'vagrant',
-  region: 'vagrant',
-  version: '1.0.0',
+  name:         'math',
+  username:     'koding',
+  environment:  'vagrant',
+  region:       'vagrant',
+  version:      '1.0.0',
   api: {
 
     square: function (x, callback) {
       callback(null, x * x);
     }
 
-  }//,
-  // logLevel: logLevels.DEBUG
+  },
+  logLevel: argv.v ? logLevels.DEBUG : logLevels.INFO
 });
 
 math.listen(5647);
 
-math.register('ws://localhost:4000',
-  fetchRegistrationIp('vagrant'),
-  fs.createReadStream(joinPath(__dirname, './kite.key'), 'utf-8')
-).then(null, warn);
+var registationIp = require('../fetch-registration-ip.js')(argv.c ? argv.c : 'vagrant');
+
+var kiteKey = fs.createReadStream(joinPath(__dirname, './kite.key'), 'utf-8');
+
+math.register('ws://localhost:4000', registationIp, kiteKey);
