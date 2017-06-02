4e6baa6f7f8cf94fedb0a3bc4e8811b30515607d
Gokmen Goksel <gokmen@goksel.me>
2017-06-02 16:54:50 -0700

    Kontrol/getPath: move to it's own file

--- src/kontrol/base.js
+++ src/kontrol/base.js
@@ -1,25 +1,7 @@
 const Emitter = require('events').EventEmitter
 const Kite = require('../kite/base')
 const KiteError = require('../kite/error')
-
-function getPath(query) {
-  const val = query.username
-  const username = val != null ? val : ''
-  const val1 = query.environment
-  const environment = val1 != null ? val1 : ''
-  const val2 = query.name
-  const name = val2 != null ? val2 : ''
-  const val3 = query.version
-  const version = val3 != null ? val3 : ''
-  const val4 = query.region
-  const region = val4 != null ? val4 : ''
-  const val5 = query.hostname
-  const hostname = val5 != null ? val5 : ''
-  const val6 = query.id
-  const id = val6 != null ? val6 : ''
-
-  return `${username}/${environment}/${name}/${version}/${region}/${hostname}/${id}`
-}
+const getPath = require('./getpath')
 
 class Kontrol extends Emitter {
   constructor(options) {
