547a3d881b6593a1bcc66a0fb7d89f21109e2bb9
Gokmen Goksel <gokmen@goksel.me>
2017-06-01 18:20:27 -0700

    Constants: to keep it up under one constants includes everything needed

---
+++ src/constants.test.js
@@ -0,0 +1,22 @@
+const expect = require('expect')
+const constants = require('./constants')
+
+describe('constants', () =>
+  it('should expose required constants', () => {
+    expect(constants.Event).toExist()
+    expect(Object.keys(constants.Event)).toEqual(constants.KnownEvents)
+
+    expect(constants.AuthType).toExist()
+    expect(constants.AuthType.token).toExist()
+
+    expect(constants.TimerHandles).toExist()
+    expect(constants.TimerHandles.includes('heartbeatHandle')).toBe(true)
+
+    expect(constants.WhiteList).toExist()
+    expect(constants.WhiteList.includes('kite.ping')).toBe(true)
+
+    expect(constants.State.NOTREADY).toBe(0)
+    expect(constants.State.READY).toBe(1)
+    expect(constants.State.CLOSED).toBe(3)
+    expect(constants.State.CONNECTING).toBe(5)
+  }))
