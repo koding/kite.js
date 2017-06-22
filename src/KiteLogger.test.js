b11792cb8c71ab1c9a9a1e9603d2e3f538821982
Umut Sirin <umutsirin1@gmail.com>
2017-06-22 19:06:23 +0300

    add KiteLogger class
    Signed-off-by: Umut Sirin <umutsirin1@gmail.com>

---
+++ src/KiteLogger.test.js
@@ -0,0 +1,35 @@
+import expect from 'expect'
+import Logger from './KiteLogger'
+import { DebugLevel } from './constants'
+
+describe('Logger', () => {
+  it('works', () => {
+    const logs = {
+      error: [],
+      warn: [],
+      info: [],
+    }
+
+    const logger = new Logger({
+      name: 'foobar',
+      level: DebugLevel.DEBUG,
+      error: (...args) => logs.error.push(args),
+      warn: (...args) => logs.warn.push(args),
+      info: (...args) => logs.info.push(args),
+    })
+
+    logger.critical('one')
+    logger.error('two')
+    logger.warning('three')
+    logger.notice('four')
+    logger.info('five')
+    logger.debug('six')
+
+    expect(logs.error[0]).toEqual(['[foobar]', 'CRITICAL', 'one'])
+    expect(logs.error[1]).toEqual(['[foobar]', 'ERROR', 'two'])
+    expect(logs.warn[0]).toEqual(['[foobar]', 'WARNING', 'three'])
+    expect(logs.info[0]).toEqual(['[foobar]', 'NOTICE', 'four'])
+    expect(logs.info[1]).toEqual(['[foobar]', 'INFO', 'five'])
+    expect(logs.info[2]).toEqual(['[foobar]', 'DEBUG', 'six'])
+  })
+})
