675844b1a6fa3c2bfeb6c0b6c05672d0681d8384
Umut Sirin <umutsirin1@gmail.com>
2017-07-31 18:12:15 +0300

    handleMessage: remove unnecessary unpacking of old kite requests
    Signed-off-by: Umut Sirin <umutsirin1@gmail.com>

--- src/kite/handleIncomingMessage.js
+++ src/kite/handleIncomingMessage.js
@@ -22,16 +22,10 @@ export default function handleIncomingMessage(proto, message) {
     return proto.handle(req)
   }
 
-  const { links, method, callbacks } = req
-  const {
-    withArgs = [],
-    authentication: auth,
-    responseCallback,
-  } = parseKiteReq(req)
+  const auth = getRequestAuth(req)
+  const responseCallback = getRequestCallback(req)
 
-  this.emit(Event.debug, 'Authenticating request')
-
-  return handleAuth(this, method, auth, this.key)
+  return handleAuth(this, req.method, auth, this.key)
     .then(token => {
       this.emit(Event.debug, 'Authentication passed')
 
@@ -41,18 +35,17 @@ export default function handleIncomingMessage(proto, message) {
       this.currentToken = token
 
       try {
-        proto.handle(
-          toProtoReq({
-            method,
-            withArgs,
-            responseCallback,
-            links,
-            callbacks,
-          })
-        )
+        proto.handle(req)
       } catch (err) {
         this.emit(Event.debug, 'Error processing request', err)
-        proto.handle(getTraceReq({ err, responseCallback, links, callbacks }))
+        proto.handle(
+          getTraceReq({
+            err,
+            responseCallback,
+            links: req.links,
+            callbacks: req.callbacks,
+          })
+        )
       }
 
       this.currentToken = null
@@ -62,66 +55,32 @@ export default function handleIncomingMessage(proto, message) {
       this.emit(Event.debug, 'Authentication failed', err)
 
       return proto.handle(
-        getTraceReq({ err, responseCallback, links, callbacks })
+        getTraceReq({
+          err,
+          responseCallback,
+          links: req.links,
+          callbacks: req.callbacks,
+        })
       )
     })
 }
 
 const isKiteReq = req =>
   req.arguments.length &&
-  req.arguments[0] &&
-  req.arguments[0].responseCallback &&
-  req.arguments[0].withArgs
+  req.arguments[req.arguments.length - 1] &&
+  req.arguments[req.arguments.length - 1].kite
 
 const getTraceReq = o => {
   return {
     method: 'kite.echo',
     arguments: [o.err, o.responseCallback],
     links: o.links,
-    callbacks: mungeCallbacks(o.callbacks, 1),
+    callbacks: o.callbacks,
   }
 }
 
-const parseKiteReq = req => req.arguments[0]
+const getRequestAuth = req =>
+  req.arguments.length && req.arguments[req.arguments.length - 1].authentication
 
-const isResponseCallback = callback =>
-  Array.isArray(callback) && callback.join('.') === '0.responseCallback'
-
-const mungeCallbacks = (callbacks, n) => {
-  // FIXME: this is an ugly hack; there must be a better way to implement it:
-
-  for (let key of Object.keys(callbacks || {})) {
-    const callback = callbacks[key]
-    if (isResponseCallback(callback)) {
-      callbacks[key] = [n]
-    }
-    if (callback[1] === 'withArgs') {
-      // since we're rewriting the protocol for the withArgs case,
-      // we need to remove everything up to withArgs
-      callbacks[key] = callback.slice(2)
-    }
-  }
-
-  return callbacks
-}
-
-const toProtoReq = ({
-  method,
-  withArgs,
-  responseCallback,
-  links,
-  callbacks,
-}) => {
-  if (!Array.isArray(withArgs)) {
-    withArgs = [withArgs]
-  }
-
-  mungeCallbacks(callbacks, withArgs.length)
-
-  return {
-    method,
-    arguments: [...Array.from(withArgs), responseCallback],
-    links,
-    callbacks,
-  }
-}
+const getRequestCallback = req =>
+  req.arguments.length && req.arguments[req.arguments.length - 2]
