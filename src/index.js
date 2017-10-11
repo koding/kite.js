fe96147c64a2594a36d416f26720bfca3f322870
Gokmen Goksel <gokmen@goksel.me>
2017-10-10 17:43:44 -0700

    Expose KiteApi

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Kite from './kite'
+import KiteApi from './kiteapi'
 import Kontrol from './kontrol'
 import KiteServer from './server'
 import { default as DnodeProtocol } from 'dnode-protocol'
 
 export default {
   Kite,
+  KiteApi,
   Kontrol,
   KiteServer,
   DnodeProtocol,
--- src/index.test.js
+++ src/index.test.js
@@ -4,6 +4,7 @@ import kite from '../src'
 describe('kite.js', () =>
   it('should provide Kontrol, Kite and KiteServer', () => {
     expect(kite.Kite).toExist()
+    expect(kite.KiteApi).toExist()
     expect(kite.Kontrol).toExist()
     expect(kite.KiteServer).toExist()
   }))
